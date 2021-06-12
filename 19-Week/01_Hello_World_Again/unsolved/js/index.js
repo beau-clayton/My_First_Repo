969f94ab2fe77fc9a6685e745e489ea2f13c6a6a
beau-clayton <77712899+beau-clayton@users.noreply.github.com>
2021-06-12 17:58:22 -0400

    in class assignments

---
+++ 19-Week/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,7 @@
+// Create a variable below
+
+var helloWorld = 'Hello World!';
+
+console.log(helloWorld);
+
+// Console.log() your variable here.
---
+++ 19-Week/05_Build_a_toggle/unsolved/js/index.js
@@ -0,0 +1,8 @@
+// Add Event Listener Below
+$("#toggle").on("click", function(){
+    $("#item1").slideToggle();
+    $("#item2").slideToggle();
+    $("#item3").slideToggle();
+}
+
+)
